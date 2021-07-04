e200b8103a0b4293fd028963a19c0f7463f999eb
akinniyThomas <akinniyithomas@yahoo.com>
2021-07-04 05:33:40 +0100

    initial setup

---
+++ src/features/header/headerSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    
+}
+
+export const headerSlice = createSlice({
+    name: "header",
+    initialState,
+    reducers:{
+
+    }
+});
+
+export const {} = headerSlice.actions;
+
+export default headerSlice.reducer;
