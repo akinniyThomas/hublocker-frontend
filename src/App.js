cdf96620459586b5270f8bdbd850d70c7605d6c6
akinniyThomas <akinniyithomas@yahoo.com>
2021-07-04 07:30:08 +0100

    header completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from './features/header/Header';
 
 function App() {
   return (
     <div className="App">
         {/* <Counter /> */}
-        <h1>Welcome To HubLocker!!!</h1>
+        {/* <h1>Welcome To HubLocker!!!</h1> */}
+        <Header/>
     </div>
   );
 }
---
+++ src/features/header/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import "./header.scss"
+import logo from "../../assets/hublogo.PNG"
+
+function Header() {
+    return (
+        <div className="header">
+            {/* <h1>Header</h1> */}
+            <div className="left">
+                <div className="logo">
+                    <img src={logo} alt="hublocker" />
+                </div>
+                <div className="menu">
+                    <ul className="menulist">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Find a Locker</a></li>
+                        <li><a href="#">Size Guide</a></li>
+                        <li><a href="#">Locations</a></li>
+                        <li><a href="#">Help Center</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="right">
+                <div className="billButton">
+                    <a href="#">PAY YOUR BILL</a>
+                </div>
+                <div className="phoneNumber">080-188-0872</div>
+                <div className="account">
+                    <a href="#">My Account</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
